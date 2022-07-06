d664776f7185757b9b8ebef3d01cb8bbf7457600
AdemirRamos <ademirramos634@gmail.com>
2022-07-05 22:54:33 -0300

    Adição da Aula 03
    Neste 'commit' foi adicionada a aula 03.

---
+++ Victor Lima - Guia do Programador/Aula 03/aula03.js
@@ -0,0 +1,19 @@
+//Aula 03 - Node Na Prática
+
+//Dica: como abrir diretórios no "cmd": "cd (arraste a página e a posicione logo após 'cd')" - caso não queira digitar o endereço inteiro.
+
+console.log('Olá, mundo!')
+
+var nome = 'Ademir'
+var idade = 25
+
+console.log(`\nNome: ${nome} e idade: ${idade}.`)
+
+var n1 = 10
+var n2 = 20
+
+function soma(a, b) {
+    return a + b
+}
+
+console.log('\nA soma tem como resultado: ', soma(n1, n2) + '.')
