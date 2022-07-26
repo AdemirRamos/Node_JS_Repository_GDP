13873014f088905090d6ddf3ceb493b6a1529694
AdemirRamos <ademirramos634@gmail.com>
2022-07-25 22:53:43 -0300

    Adição da Aula 17
    Neste 'commit' foi adicionada a aula 17 sobre conexão com bancos de dados.

---
+++ Victor Lima - Guia do Programador/Aula 17/aula17.js
@@ -0,0 +1,24 @@
+//Aula 17 - Como Se Conectar Ao MySQL
+
+//O Sequelize é um ORM; isso quer dizer que ele abstrai toda a camada dos banco de dados;
+//Dessa forma, não é necessário digitar longas "query". Exemplo: SELECT * FROM usuários WHERE nome = "Ademir".
+
+//Conectando-se a um banco de dados:
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('teste', 'root', '123456', {host: "localhost", dialect: "postgresql"})
+
+//Parâmetros:
+//1º: Nome do banco de dados;
+//2º: nome do usuário do banco de dados;
+//3º: senha;
+//4º: indicação do "host" dentro de um "JSON";
+//5º: dialect: indica o administrador de banco de dados ao qual se está fazendo a conexão (também dentro do JSON).
+
+//Testando a conexão:
+
+Sequelize.authenticate().then(() => {console.log('Conexão feita com sucesso!')}).catch(function (erro) {console.log(`Erro ao se conectar! Erro: ${erro}`)})
+
+//Através das funções "then()" e "catch()" é possível gerar um retorno em caso de sucesso ou insucesso ao se conectar ao banco de dados.
+
+//"then()" é uma função de "callback" (retorno) que é executada quando algum evento acontece.
