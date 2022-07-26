2a3b5ad7ad0eb4054e0721bedac9a364f8b518c0
AdemirRamos <ademirramos634@gmail.com>
2022-07-25 22:26:25 -0300

    Adição da Aula 15
    Neste 'commit' foi adicionada a aula 15 sobre atualização de registros.

---
+++ Victor Lima - Guia do Programador/Aula 15/aula15.js
@@ -0,0 +1,10 @@
+//Aula 15 - Atualizando Registros No MySQL
+
+
+/*
+
+	UPDATE usuários SET nome = "Ademir", email = "teste123@gmail.com" -> atualiza todos os registros do banco de dados.
+
+	UPDATE usuários SET nome = "Ademir" WHERE nome = "Ramos" -> atualiza só o registro especificado.
+
+*/
