f3b8de98bfd5799f0cb40bfd81b8f602322e078d
AdemirRamos <ademirramos634@gmail.com>
2022-07-25 22:20:00 -0300

    Adição da Aula 14
    Neste 'commit' foi adicionada a aula 14 sobre deletar registros em tabelas.

---
+++ Victor Lima - Guia do Programador/Aula 14/aula14.js
@@ -0,0 +1,9 @@
+//Aula 14 - Deletando Registros No MySQL
+
+/*
+
+	DELETE FROM usuários -> exclui todos os dados da tabela.
+
+	DELETE FROM usuários WHERE nome = "Ademir" -> deleta um registro específico.
+
+*/
