095f2aa714f6caec728d63e430e2383f8af36e9f
AdemirRamos <ademirramos634@gmail.com>
2022-07-25 23:18:52 -0300

    Adição da Aula 18
    Neste 'commit' foi adicionada a aula 18 sobre 'Sequelize Models'.

---
+++ Victor Lima - Guia do Programador/Aula 18/aula18.js
@@ -0,0 +1,60 @@
+//Aula 18 - Models No Sequelize
+
+//Basicamente, um "model" é uma referência à sua tabela dentro do Sequelize.
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('teste', 'root', '123456', {host: "localhost", dialect: "postgresql"})
+
+const Postagem = sequelize.define('postagens', {
+	título: {
+		type: Sequelize.STRING //criando um campo do tipo "VARCHAR". - VARCHAR possui um limite de caracteres.
+	},
+
+	conteúdo: {
+		type: Sequelize.TEXT //criando um campo do tipo "TEXT". - TEXT não possui um limite de caracteres.
+	}
+})
+
+//A função recebida por "Postagem" cria um banco de dados; dentro do JSON, vão as tabelas desse banco de dados.
+
+//Criando o banco dados no administrador de banco de dados:
+
+Postagem.sync({force: true}) //"({force: true})" garante que uma tabela será gerada.
+
+//Inserindo dados em uma tabela:
+
+Postagem.create({
+	título: "Título de Exemplo",
+	conteúdo: "Conteúdo de Exemplo."
+})
+
+//Criando mais um "model":
+
+const Usuários = sequelize.define('usuários', {
+	nome: {
+		type: Sequelize.STRING
+	},
+
+	sobrenome: {
+		type: Sequelize.STRING
+	},
+
+	idade: {
+		type: Sequelize.INTIGER
+	},
+
+	email: {
+		type: Sequelize.STRING
+	}
+})
+
+Usuários.sync({force: true})
+
+//Uma vez que o "model" seja sincronizado com o administrador de banco de dados, recomenda-se deletar ou comentar essa linha de código.
+
+Usuários.create({
+	nome: "Sofia",
+	sobrenome: "Ramos",
+	idade: 99,
+	email: "teste123@gmail.com"
+})
