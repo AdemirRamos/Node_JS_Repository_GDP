7eaa2162f11a87337c13eddbec420b16daf159a2
AdemirRamos <ademirramos634@gmail.com>
2022-07-25 22:32:38 -0300

    Adição da Aula 16
    Neste 'commit' foi adicionada a aula 16 (instalação do Sequelize).

---
+++ Victor Lima - Guia do Programador/Aula 16/aula16.js
@@ -0,0 +1,6 @@
+//Aula 16 - Sequelize
+
+//O Sequelize nos permite trabalhar com diferentes administradores de banco de dados (e com os bancos de dados em si).
+
+//Para além de instalar o Sequelize, também é necessário instalar o administrador do banco de dados;
+//Para isso, é necessário ir até a página do NPM e buscar pelo código de instalação do administrador (de banco de dados) desejado.
